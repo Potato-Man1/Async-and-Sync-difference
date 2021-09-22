8c5382524a58486ed9585ee6635c783599c6fb3d
Potato-Man1 <89878281+Potato-Man1@users.noreply.github.com>
2021-09-22 19:20:26 +0800

    Create Difference.py

---
+++ Difference.py
@@ -0,0 +1,24 @@
+import asyncio, time
+#The sleep is just copying an action that takes some time to finish
+def foo():
+    for i in range(10):
+        time.sleep(1)
+        print(1)
+
+def fee():
+    print('start')
+    time.sleep(5)
+    print('done!')
+    
+def fees():
+    print('starts')
+    time.sleep(4)
+    print('dones!')
+    
+
+def main():
+    fee()
+    foo()
+    fees()
+
+main()
