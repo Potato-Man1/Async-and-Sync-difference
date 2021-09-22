c8d6e40134b33c66dc3faebfeb808c2faebd79b0
Potato-Man1 <89878281+Potato-Man1@users.noreply.github.com>
2021-09-22 20:22:45 +0800

    Update sync.py

--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
 import time
 #The sleep is just copying an action that takes some time to finish
+#This part of the file is not needed for any explanation.
 def foo():
     for i in range(10):
         time.sleep(1)
@@ -14,7 +15,6 @@ def fees():
     print('start task 2')
     time.sleep(4)
     print('task 2 done!')
-    
 
 def main():
     fee()
